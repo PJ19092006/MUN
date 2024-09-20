8a5b239dd308f3d70fbddc5ce42e05e053d7bad5
Parjanay <169073876+PJ19092006@users.noreply.github.com>
2024-09-20 21:22:46 +0530

    Create mailerIndividual.js

---
+++ public/js/mailerIndividual.js
@@ -0,0 +1,35 @@
+const nodemailer = require("nodemailer");
+
+async function mailerIndividual(
+  recipientEmail,
+  recipientName,
+  Portfolio,
+  committeeAllotted
+) {
+  // Create a reusable transporter object using the default SMTP transport
+  const transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    auth: {
+      user: "YOUR_USER",
+      pass: "YOUR_PASS",
+    },
+  });
+
+  // Define the email options
+  let mailOptions = {
+    from: '"MUN Team" <sggsmun2024@gmail.com>', // sender address
+    to: recipientEmail, // list of receivers
+    subject: "Committee Allotment for SGGS MUN 2024", // Subject line
+    text: `Dear ${recipientName},\n\nYou have been allotted the committee: ${committeeAllotted}. and your Portfolio:${Portfolio} \n\nBest regards,\nMUN Team`, // plain text body
+  };
+
+  // Send mail with defined transport object
+  let info = await transporter.sendMail(mailOptions);
+
+  console.log("Message sent: %s", info.messageId);
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+}
+
+module.exports = mailerIndividual;
